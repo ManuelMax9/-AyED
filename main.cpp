54554960566a3e9c598d775dae3449ef00943f5e
Manuel Max <mmaxjimenez@frba.utn.edu.ar>
2026-05-15 20:23:19 -0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string nombre = "Manuel";
+    string apellido = "Max";
+
+    cout << "Hola " << nombre << " " << apellido << endl   ;
+
+    return 0;
+}
